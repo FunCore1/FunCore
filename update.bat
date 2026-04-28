d31eaea74ca67df8738f8e2e75d6aec8fe1c0554
Ayush <akukreti784@gmail.com>
2026-04-28 09:42:38 +0530

    Update: 28-04-2026  9:42:38.55

--- update.bat
+++ update.bat
@@ -1,5 +1,12 @@
 @echo off
+echo Adding files...
 git add .
-git commit -m "auto update"
+
+echo Committing changes...
+git commit -m "Update: %date% %time%" || echo No changes to commit
+
+echo Pushing to GitHub...
 git push
+
+echo Done!
 pause
